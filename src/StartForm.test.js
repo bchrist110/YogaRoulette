9b5964f3fbc3506b2428388b88b7bac1631a78c9
Brandon Christianson <b.christianson110@gmail.com>
2021-03-12 10:58:51 -0800

    3/12 tests

--- src/StartForm.test.js
+++ src/StartForm.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import StartForm from './StartForm';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<StartForm />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
